80bfa45b199f9bee2f7e5d0a5ae6250773ab524d
sophiekumagai <sophie.satie20@gmail.com>
2024-06-21 14:42:35 -0300

    alterando os arquivos

--- app.py
+++ app.py
@@ -1,30 +1,12 @@
-from flask import Flask, render_template_string
+from flask import Flask, render_template, render_template_string, request, jsonify
+from flask_cors import CORS
+import pandas as pd
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    html_code = '''
-    <!DOCTYPE html>
-    <html lang="pt-BR">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Abrir Câmera</title>
-    </head>
-    <body>
-        <button onclick="openCamera()">Abrir Câmera</button>
-
-        <script>
-            function openCamera() {
-                const cameraUrl = 'intent://#Intent;action=android.media.action.IMAGE_CAPTURE;end';
-                window.location.href = cameraUrl;
-            }
-        </script>
-    </body>
-    </html>
-    '''
-    return render_template_string(html_code)
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
